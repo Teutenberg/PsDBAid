21e24c1c5a4428f13ed25cf98685e6ce8c2d54a4
Wayne Teutenberg <Wayne_Teutenberg@bnz.co.nz>
2019-09-11 15:34:02 +1200

    init

---
+++ PsDBAid.psm1
@@ -0,0 +1,8 @@
+<#
+    PsDBAid.psm1 is a helper module for scripted activities. 
+    This module will contain functions to help simplify scripting and increase productivity. 
+#>
+
+Import-Module SqlServer
+Import-Module SqlServerDsc
+#Import-Module ReportingServicesTools
