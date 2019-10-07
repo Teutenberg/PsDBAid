7f80571aa461d8b25e2881486c1b2219f6fe8b15
Wayne Teutenberg <Wayne_Teutenberg@bnz.co.nz>
2019-10-07 15:38:51 +1300

    Add test for connect-sqlserver

--- tests/Connect-SqlServer.test.ps1
+++ tests/Connect-SqlServer.test.ps1
@@ -6,12 +6,37 @@ $script:module = Join-Path -Path $script:modulePath -ChildPath 'PsDBAid.psm1'
 Import-Module -Name $script:module -Force
 
 Describe 'Connect-SqlServer' {
+    BeforeAll {
+        $TestWinCred = Get-Credential -Message "Please supply windows credential to impersonate."
+        $TestSqlCred = Get-Credential -Message "Please supply sql login to impersonate."
+    }
+
     Context 'When connecting using integrated authentication' {
-        It 'Should return the correct service instance' {
+        It 'Should connect with integrated security' {
             $Smo = Connect-SqlServer
             $Smo.ConnectionContext.ServerInstance | Should -BeExactly $env:COMPUTERNAME
+            $Smo.Status | Should -Match '^Online$'
+        }
 
-            #Assert-MockCalled -CommandName New-Object -Exactly -Times 1 -Scope It -ParameterFilter $mockNewObject_MicrosoftDatabaseEngine_ParameterFilter
+        It 'Should connect with sql authentication' {
+            $Smo = Connect-SqlServer -Credential $TestSqlCred
+            $Smo.ConnectionContext.ServerInstance | Should -BeExactly $env:COMPUTERNAME
+            $Smo.ConnectionContext.LoginSecure | Should -BeExactly $false
+            $Smo.ConnectionContext.Login | Should -BeExactly $TestSqlCred.UserName
+            $Smo.Status | Should -Match '^Online$'
+        }
+
+        It 'Should connect with impersonated windows authentication' {
+            $Smo = Connect-SqlServer -Credential $TestWinCred
+            $Smo.ConnectionContext.LoginSecure | Should -BeExactly $true
+            $Smo.ConnectionContext.ConnectAsUser | Should -BeExactly $true
+            $Smo.ConnectionContext.ConnectAsUserName | Should -BeExactly $TestWinCred.GetNetworkCredential().UserName
+            $Smo.ConnectionContext.ConnectAsUserPassword | Should -BeExactly $TestWinCred.GetNetworkCredential().Password
+            $Smo.Status | Should -Match '^Online$'
+        }
+
+        It 'Should throw error when server does not exist' {
+            { Connect-SqlServer -SqlServer 'IDONOTEXIST' -ErrorAction Stop } | Should -Throw "Failed to connect to SQL instance IDONOTEXIST"
         }
     }
 }
